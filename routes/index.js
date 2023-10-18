a905ce1da7721ffd7387c12e970ff82ac117eab2
nandonyata <nandonp02@gmail.com>
2023-10-18 20:12:50 +0700

    feat: adding edit route & gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var express = require("express");
 var router = express.Router();
 const stock_read_log = require("../models/stock_read_log");
@@ -25,22 +26,14 @@ router.use("/export-data", async (req, res) => {
 });
 
 router.use("/import-data", async (req, res) => {
-  const list = await stock_read_log
-    .aggregate([
-      {
-        $match: {},
-      },
-    ])
-    .exec();
-
   FileSystem.readFile("./stock_read_log.json", async (error, data) => {
     if (error) throw error;
 
     const list = JSON.parse(data);
 
-    const deletedAll = await stock_read_log.deleteMany({});
+    await stock_read_log.deleteMany({});
 
-    const insertedAll = await stock_read_log.insertMany(list);
+    await stock_read_log.insertMany(list);
 
     console.log("stock_read_log.json imported!");
     res.json({ statusCode: 1, message: "stock_read_log.json imported!" });
@@ -49,6 +42,49 @@ router.use("/import-data", async (req, res) => {
 
 router.use("/edit-repacking-data", async (req, res) => {
   // Silahkan dikerjakan disini.
+  const { company_id, payload, reject_qr_list, new_qr_list } = req.body;
+
+  const newList = new_qr_list.map((e) => e.payload);
+  const rejectedList = reject_qr_list.map((e) => e.payload);
+
+  let [findNewList, findStock] = await Promise.all([
+    stock_read_log.find({
+      "qr_list.payload": { $in: newList },
+    }),
+    stock_read_log.findOne({ company_id, payload }),
+  ]);
+
+  findStock.qr_list = findStock.qr_list.filter(
+    (e) => !rejectedList.includes(e.payload)
+  );
+
+  const updateStock = [];
+  for (let i = 0; i < findNewList.length; i++) {
+    const el = findNewList[i];
+    const excludeQty = el.qr_list.filter((e) => !newList.includes(e.payload));
+    const includeQty = el.qr_list.filter((e) => newList.includes(e.payload));
+
+    findStock.qr_list = [...findStock.qr_list, ...includeQty];
+
+    updateStock.push(
+      stock_read_log.updateOne(
+        { payload: el.payload },
+        { $set: { qr_list: excludeQty, qty: excludeQty.length } }
+      )
+    );
+  }
+
+  findStock.qty = findStock.qr_list.length;
+  updateStock.push(findStock.save());
+
+  await Promise.all(updateStock);
+
+  res.json({
+    statusCode: 1,
+    message: "stock_read_log.json edited!",
+    findStock,
+    findNewList,
+  });
 });
 
 router.use("/", function (req, res, next) {
